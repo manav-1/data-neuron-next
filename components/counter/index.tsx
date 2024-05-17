057fc67f4069d4904d5075e41d5844a4ba17f3e0
Manav Arora <manav81101@gmail.com>
2024-05-17 23:53:57 +0530

    Added API's for add count and update

--- components/counter/index.tsx
+++ components/counter/index.tsx
@@ -1,24 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "@/components/counter/styles.module.css";
+import client from "@/client/axios";
 
 type InputComponentProps = {
   id: string;
 };
 
-const InputComponent = ({ id }: InputComponentProps) => {
+export default function InputComponent({ id }: InputComponentProps) {
   const [inputValue, setInputValue] = useState("");
+  const [count, setCount] = useState<{
+    addCount: 0;
+    updateCount: 0;
+  } | null>(null);
 
-  // Simulate fetching value from API
-  useEffect(() => {
-    // Replace with actual API call
-    const fetchValueFromAPI = async () => {};
+  const handleAdd = () => {
+    try {
+      client.post(`/`, { data: inputValue, instanceId: id });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    fetchValueFromAPI();
-  }, []);
+  const handleUpdate = () => {
+    try {
+      client.put(`/${id}`, { data: inputValue, instanceId: id });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  const handleAdd = () => {};
-
-  const handleUpdate = () => {};
+  const getCount = async () => {
+    try {
+      const { data: count } = await client.get(`${id}/count`);
+      console.log(count);
+      setCount(count as any);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className={styles.counterContainer}>
@@ -29,12 +48,17 @@ const InputComponent = ({ id }: InputComponentProps) => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
+      {count && (
+        <div>
+          {<p>Add Count: {count.addCount}</p>}
+          {<p>Update Count: {count.updateCount}</p>}
+        </div>
+      )}
       <div className={styles.counterButtons}>
         <button onClick={handleAdd}>Add</button>
         <button onClick={handleUpdate}>Update</button>
+        <button onClick={getCount}>Get Count</button>
       </div>
     </div>
   );
-};
-
-export default InputComponent;
+}
